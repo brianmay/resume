74867a57e5cd42ff77c59b2ed46871ba990be378
Brian May <brian@linuxpenguins.xyz>
2016-02-11 10:02:30 +1100

    Update HTML title.

--- conf.py
+++ conf.py
@@ -115,10 +115,10 @@ html_theme_options = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-# html_title = None
+html_title = "Resume of Brian May"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-# html_short_title = None
+html_short_title = "Brian May"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
